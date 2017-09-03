ab9565639091c284a94ffac53eb6f0404574b5bc
liuxinsi <akalxs@gmail.com>
2017-09-03 20:12:12 +0800

    事务 配置文件密码加密

--- rest-service/src/main/java/com/lxs/mms/db/service/UserService.java
+++ rest-service/src/main/java/com/lxs/mms/db/service/UserService.java
@@ -2,14 +2,17 @@ package com.lxs.mms.db.service;
 
 import com.lxs.mms.db.bean.User;
 import com.lxs.mms.db.mapper.UserMapper;
+import com.lxs.mms.utils.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author liuxinsi
  * @mail akalxs@gmail.com
  */
 @Service
+@Transactional
 public class UserService {
     @Autowired
     private UserMapper userMapper;
@@ -18,6 +21,7 @@ public class UserService {
         if (user.getStatus() == null) {
             user.setStatus(User.Status.ACTIVE);
         }
+        user.setUserId(new ObjectId().toString());
         return userMapper.saveUser(user);
     }
 }
